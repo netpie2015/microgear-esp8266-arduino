bc933a5c388f1e50c886a404e79a8b9ac742bfff
chavee <i@chavee.com>
2016-11-22 13:13:28 +0700

    implement function microgear.writeFeed()

--- MicroGear.h
+++ MicroGear.h
@@ -67,6 +67,8 @@
 #define ABSENT                     3
 #define CONNECTED                  4
 #define CALLBACK                   5
+#define ERROR          		       6
+#define INFO                  	   7
 
 
 class MicroGear {
@@ -121,6 +123,10 @@ class MicroGear {
 		bool publish(char*, String, bool);
 		bool publish(char*, String, String);
 		bool publish(char*, String, char*);
+
+		bool writeFeed(char*, char*);
+		bool writeFeed(char*, char*, char*);
+		bool writeFeed(char*, String);
 		bool writeFeed(char*, String, char*);
 
 		bool chat(char*, char*);
